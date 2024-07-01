83b559956463e7472ce131e9900d141d05e007d8
rgb234 <fehs0611@gmail.com>
2024-07-01 17:04:27 +0900

    .env

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatModule } from './chat/chat.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        // With that option specified, every entity registered through the forFeature() method
+        // will be automatically added to the entities array of the configuration object.
+        autoLoadEntities: true,
+        // entities: [Authentication, User, Room, UserToRoom, Message],
+        retryAttempts: 3,
+      }),
+      inject: [ConfigService],
+    }),
+    ChatModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
